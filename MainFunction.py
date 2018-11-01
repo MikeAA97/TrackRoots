9adc23899035df6eddb75539443a74360edd38f0
Michael Austin <44247380+MikeAA97@users.noreply.github.com>
2018-11-01 09:58:46 -0400

    Add files via upload

--- MainFunction.py
+++ MainFunction.py
@@ -44,88 +44,162 @@ def toGreenJPG(filePath):   #Read in filepath
     
     return fileJPG
     
-   
+#Create a function that plots a point and coordinates on the image  
+def clickAndPrint(event):
+    
+    global count
+    global listOfXC
+    global listOfYC     
+    global listOfP
+    global listOfI
+    global PREimg2
+    global image2
+    #outputting x and y coords to console
+    pos = ("Point " + str(count))
+    #add poistion to list
+    listOfP.append(pos)
+    #get coordinates on mouse click
+    x = event.x
+    y = event.y
+    # add coordinates to lists
+    listOfXC.append(x)
+    listOfYC.append(y)
+    
+    #convert image to numpy array
+    imgD = np.asarray(image2)
+    imgDS = imgD[:,:,0]
+    #get intensity from coordinate of mouseclick
+    startIntensity = 249 #imgDS[y][x]
+    listOfI.append(imgDS[y][x])
+    print("starting intensity is ", str(startIntensity))
+    print (pos, "coordinates are: ", str(x),",",str(y))
+    #draw point 
+    w.create_oval(x-3,y-3, x+3, y+3, fill="yellow")
+    #add text to point
+    if(count > -1):
+        w.create_text(x+20,y-5,text=("Point " + str(count)))
+        count +=1 
+        
+    ###Track root
+    #Base Case
+    b = 0
+    r = isRoot(x,y,imgDS, startIntensity, b)
+    #recursion
+    while( r != ''):
+        if(r=='left'):
+            x-=1
+            y+=1
+            n = isRoot(x,y,imgDS,startIntensity, b)
+        elif(r == 'right'):
+            x+=1
+            y+=1
+            n = isRoot(x,y, imgDS, startIntensity, b)
+        elif(r == 'down'):
+            y+=2
+            n = isRoot(x,y, imgDS, startIntensity,b)
+        r = n
 
+def isRoot(xcoor, ycoor, imgDS, startIntensity, buffer):
+     #check left pixel
+    global count
+    checkNext = ''
+    if imgDS[ycoor][xcoor-1] <= startIntensity+buffer:
+        newx = xcoor-1
+        pos = ("Point " + str(count))
+        listOfP.append(pos)
+        listOfXC.append(newx)
+        listOfYC.append(ycoor)
+        listOfI.append(imgDS[ycoor][newx])
+        count +=1 
+        print ("left coordinates are: ", str(newx),",",str(ycoor))
+        w.create_oval(newx-3,ycoor-3, newx+3, ycoor+3, fill="yellow")
+        #w.create_text(newx+20,ycoor-5,text=("added"))
+        checkNext = 'left'
+        return checkNext
+    # check right pixel
+    if imgDS[ycoor][xcoor+1] <= startIntensity+buffer:
+        newx = xcoor+1
+        pos = ("Point " + str(count))
+        listOfP.append(pos)
+        listOfXC.append(newx)
+        listOfYC.append(ycoor)
+        listOfI.append(imgDS[ycoor][newx])
+        count +=1 
+        print ("right coordinates are: ", str(newx),",",str(ycoor))
+        w.create_oval(newx-3, ycoor-3, newx+3, ycoor+3, fill="yellow")
+        #w.create_text(newx+20,ycoor-5,text=("added"))
+        checkNext = 'right'
+        return checkNext
+    # check down pixel
+    if imgDS[ycoor-1][xcoor] <= startIntensity+buffer:
+        newy = ycoor + 1
+        pos = ("Point " + str(count))
+        listOfP.append(pos)
+        listOfXC.append(xcoor)
+        listOfYC.append(newy)
+        listOfI.append(imgDS[newy][xcoor])
+        count +=1 
+        print ("down coordinates are: ", str(xcoor),",",str(newy))
+        w.create_oval(xcoor-3,newy-3, xcoor+3, newy+3, fill="yellow")
+        #w.create_text(xcoor+20,newy-5,text=("added"))
+        checkNext = 'down'
+        return checkNext
+    else:
+        print("Not Working") 
+    # return what point to check next in recursion
+    return checkNext
+        
+
+############################################################################
+
+###-Run GUI
 master = Tk()
+
 #Set up initial image variables
-imagePath1 = askopenfilename()
-imagePath2 = askopenfilename()
- #Convert the images into a JPG for use in Tkinter window
+imagePath1 = 'C:/Users/loves/OneDrive/Desktop/TrackRoots/071217-pos1,3-dr5;pos2,4-cle44-luc-1-5/pos1,3-dr5_pos2,4-cle44-luc-1-5_w1[None]_s1_t1.TIF' #askopenfilename()
+imagePath2 = 'C:/Users/loves/OneDrive/Desktop/TrackRoots/071217-pos1,3-dr5;pos2,4-cle44-luc-1-5/pos1,3-dr5_pos2,4-cle44-luc-1-5_w1[None]_s1_t49.TIF' #askopenfilename()
+#Convert the images into a JPG for use in Tkinter window
 image1 = Image.open(toGreenJPG(imagePath1))
 image2 = Image.open(toOrangeJPG(imagePath2))
- #Set up structure for window
-    
+# Window size
 width = 1024
-height = 1024  #Create an image object to initialize the size of the canvas window
- #Create the external windoww
- #Convert images from RGB to RGBA 
+height = 1024 
+#Convert images from RGB to RGBA 
 PREimg = image1.convert("RGBA")
 PREimg2 = image2.convert("RGBA")
+#Set alpha valu for images such that the top one is more transparent
 PREimg.putalpha(255)
 PREimg2.putalpha(128)
-print(PREimg.size)
-print(PREimg2.size)
- #Merge the two images
-c = Image.blend(PREimg, PREimg2, 0.42)
- #Counter for mouseclick events
+# print(PREimg.size)
+# print(PREimg2.size)
+
+
+#Counter for mouseclick events
 count = 1
 #Arrays to hold coordinate values and points
 listOfXC = []
 listOfYC = []
 listOfP = []
 listOfI = []
-w = Canvas(master, width=width, height=height) #intialize the canvas on the external window with a size of the image being displayed
+#intialize the canvas on the external window with a size of the image being displayed
+w = Canvas(master, width=width, height=height) 
 w.pack()
- #Plot the image
+
+#Plot the image
+#converting image to TK image
 img1 = ImageTk.PhotoImage(PREimg)
 img2 = ImageTk.PhotoImage(PREimg2)
+#putting TK images on canvas
 w.create_image(0,0,anchor="nw",image=img1)
 w.create_image(0,0,anchor="nw",image=img2)
-#w.create_image(20,20,anchor="nw",image=img2)
-#Create a function that plots a point and coordinates on the image
 
 
-
-
-            
-def click(event, x, y):
-    xcoor = event.x
-    ycoor = event.y
-
-    
-    def clickAndPrint(x, y):
-        #outputting x and y coords to console
-        global count
-        global listOfXC
-        global listOfYC     
-        global listOfP
-        global listOfI
-        global PREimg2
-        #imgData = np.asarray(PREimg2)
-        #imgDS = imgData[:,:,0]
-        pos = ("Point " + str(count))
-        listOfP.append(pos)
-        x, y = xcoor, ycoor
-        listOfXC.append(x)
-        listOfYC.append(y)
-        print (pos, "coordinates are: ", str(x),",",str(y))
-        w.create_oval(x-3,y-3, x+3, y+3, fill="yellow")
-        if(count > -1):
-            w.create_text(x+20,y-5,text=("Point " + str(count)))
-            count +=1
-        
-    clickAndPrint(xcoor, ycoor) 
-        
-    
-            
-         
-        
-        
-     #mouseclick event
-
-w.bind("<Button 1>",click)
+#mouseclick event    
+w.bind("<Button 1>",clickAndPrint)
+#Ends GUI
 master.mainloop()
-dataf = {'X Coordinates':listOfXC,'Y Coordinates':listOfYC}
+#Output coords
+dataf = {'X Coordinates':listOfXC,'Y Coordinates':listOfYC, 'Intensities':listOfI}
 df = pd.DataFrame(dataf, index=listOfP)
 print(df)
 df.to_csv("Test.csv")
